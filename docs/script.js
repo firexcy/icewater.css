f6d9d210474b1f1fb3d3774d5c788f64a661cae8
Platy Hsu <serv@cyhsu.xyz>
2021-07-27 22:59:25 +0800

    edit docs

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const localBase = './water.css/'
+const localBase = 'https://cdn.jsdelivr.net/gh/firexcy/icewater.css@master/out/'
 
 const fileSizes = {
   dark: 2.57,
@@ -11,7 +11,6 @@ const fileSizes = {
 const themeForm = document.getElementById('theme-form')
 const stylesheet = document.getElementById('js-stylesheet')
 const startupStylesheet = document.getElementById('js-startup-stylesheet')
-const productHunt = document.getElementById('product-hunt')
 const copyButton = document.getElementById('copy-button')
 const copyButtonFeedback = document.getElementById('copy-button-feedback')
 const linkSnippets = [].slice.call(document.querySelectorAll('#link-snippet-container > pre'))
@@ -20,13 +19,6 @@ const table = {
   fileName: document.getElementById('table-file-name'),
   fileSize: document.getElementById('table-file-size'),
   theme: document.getElementById('table-theme')
-}
-
-const prefersColorScheme = window.matchMedia('(prefers-color-scheme: light)')
-
-const updateProductHunt = (theme) => {
-  theme = theme || (prefersColorScheme.matches ? 'light' : 'dark')
-  productHunt.src = `https://api.producthunt.com/widgets/embed-image/v1/top-post-badge.svg?post_id=150490&theme=${theme}&period=daily`
 }
 
 const updateTheme = () => {
@@ -45,24 +37,16 @@ const updateTheme = () => {
   table.fileSize.innerText = `${fileSizes[theme].toFixed(2)} kb`
 
   if (theme === 'auto') {
-    updateProductHunt()
     table.theme.innerHTML = `
     Respects user-defined theme settings using <a style="--links: var(--code)" href="https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-color-scheme" target="_blank" rel="noopener"><code>prefers-color-scheme</code></a>.<br>
     Light in browsers where the theme settings can't be detected.
     `
   } else {
-    updateProductHunt(theme)
     table.theme.innerText = `Theme is forced to ${theme}.`
   }
 }
 
 themeForm.addEventListener('change', updateTheme)
-
-updateProductHunt()
-prefersColorScheme.addListener(() => {
-  if (themeForm.theme.value !== 'auto') return
-  updateProductHunt()
-})
 
 updateTheme()
 startupStylesheet.parentElement.removeChild(startupStylesheet)
